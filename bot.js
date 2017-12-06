102e706efd155f37c8ac2e40b6de7508e950bae4
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2017-12-06 13:01:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,16 @@
 
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!'
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === prefix + 'Trotsky') {
-    msg.channel.sendMessage('¿Me estás llamando revi?');
-  }
-  if (msg.content === prefix + 'help') {
-    msg.channel.sendMessage('Trotsky; Pablo;')
-  }
-  if (msg.content === prefix + 'Pablo') {
-    msg.channel.sendMessage('http://www.magazinefa.com/assets/uploads/2016/04/pablo-3-980x675.png')
-  }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-
-client.login('proces.env.BOT_TOKEN');
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
