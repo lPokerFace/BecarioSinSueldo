7650f9ca6180c4cf62f9ae7f7f88796f964c6403
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2017-12-06 13:09:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,16 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', msg => {
+  if (msg.content === prefix + 'Trotsky') {
+    msg.channel.sendMessage('¿Me estás llamando revi?');
+  }
+  if (msg.content === prefix + 'help') {
+    msg.channel.sendMessage('Trotsky /n Pablo /n');
+  }
+  if (msg.content === prefix + 'Pablo') {
+    msg.channel.sendMessage('http://www.magazinefa.com/assets/uploads/2016/04/pablo-3-980x675.png');
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
