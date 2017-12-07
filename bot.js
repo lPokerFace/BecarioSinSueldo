7953a6a4cbdfb3d79624e5350f1f50b5bd72b31f
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2017-12-08 00:46:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,16 +6,16 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-     if (message.content === prefix + 'help') {
-    	 message.sendMessage('help - Muestra los códigos del bot.');
-  	}
-     if (message.content === prefix + 'Trotsky') {
-    	 message.sendMessage(':piolet:');
-  	}
- if (message.content === prefix + 'Pablo') {
-    	 message.sendMessage('http://i61.tinypic.com/33mtlj6.png');
-  	}    
+client.on('message', msg => {
+  if (msg.content === prefix + 'Trotsky') {
+    msg.channel.sendMessage('Pioletazo');
+  }
+  if (msg.content === prefix + 'help') {
+    msg.channel.sendMessage(`help` <br/> 'Pablo' <br/> 'Trotsky')
+  }
+  if (msg.content === prefix + 'Pablo') {
+    msg.channel.sendMessage('http://www.magazinefa.com/assets/uploads/2016/04/pablo-3-980x675.png')
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
