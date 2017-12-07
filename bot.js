8585e8e6aaa6a88e23a0e265534c9a176fd33784
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2017-12-07 20:41:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!'
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === prefix + 'Trotsky') {
-    msg.channel.sendMessage('¿Me estás llamando revi?');
-  }
-  if (msg.content === prefix + 'help') {
-    msg.channel.sendMessage(`Trotsky` <br /> `Pablo`);
-  }
-  if (msg.content === prefix + 'Pablo') {
-    msg.channel.sendMessage('http://www.magazinefa.com/assets/uploads/2016/04/pablo-3-980x675.png');
-  }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
