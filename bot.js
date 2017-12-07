948df447bc622a3c18737d2089957c6c5c98f9a9
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2017-12-08 00:53:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,10 +11,10 @@ client.on('message', msg => {
     msg.channel.sendMessage('Pioletazo');
   }
   if (msg.content === prefix + 'help') {
-    msg.channel.sendMessage('help - Lista de los códigos disponibles')
+    msg.channel.sendMessage('help - Lista de los códigos disponibles');
   }
   if (msg.content === prefix + 'Pablo') {
-    msg.channel.sendMessage('http://www.magazinefa.com/assets/uploads/2016/04/pablo-3-980x675.png')
+    msg.channel.sendMessage('http://www.magazinefa.com/assets/uploads/2016/04/pablo-3-980x675.png');
   }
 });
 
