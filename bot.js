c8bdcec972ffadb2a7aeaf83735fdb1828166f7f
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2017-12-07 21:26:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '_'
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === prefix + 'ping') {
     	message.reply('pong');
   	}
 });
