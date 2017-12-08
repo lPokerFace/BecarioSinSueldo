1e9bc495e6c54f36ef3f87f2365f3ea4d4159bde
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2017-12-08 01:08:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === prefix + 'Trotsky') {
-    msg.channel.sendMessage('Pioletazo');
-    msg.channel.sendMessage('Y Fidel es un gaylo');  
+    msg.channel.sendMessage('Pioletazo'); 
   }
   if (msg.content === prefix + 'help') {
     msg.channel.sendMessage('help - Lista de los códigos disponibles');
