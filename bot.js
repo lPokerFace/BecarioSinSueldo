9b334eac1682a2839a40447028d59084a02f2d3d
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2017-12-10 22:42:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,45 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '_'
 
 client.on('ready', () => {
-    console.log('I am ready!');
+   console.log('I\'m Online \nI\'m Online')
 });
 
-client.on('message', msg => {
-  if (msg.content === prefix + 'Trotsky') {
-    msg.channel.sendMessage('<:piolet:359043347391971339>'); 
-  }
-  if (msg.content === prefix + 'help') {
-    msg.channel.sendMessage('help - Lista de los códigos disponibles');
-  }
-  if (msg.content === prefix + 'Pablo') {
-    msg.channel.sendMessage('http://www.magazinefa.com/assets/uploads/2016/04/pablo-3-980x675.png');
-  }
+var prefix = '+'
+client.on('message', message => {
+  let args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+   if (message.content.startsWith(prefix + 'ping')) {
+      message.channel.send('pong');
+   }
+
+   if (message.content.startsWith(prefix + 'help')) {
+     message.channel.send('help - Muestra una lista de todos los códigos.');
+     message.channel.send('habilidades - Muestra un resumen de la habilidades.');
+     message.channel.send('prueba - Envía un mensaje privado de prueba.')
+   }
+
+   if (message.content.startsWith(prefix + 'prueba')) {
+    message.channel.send('Mira tus mensajes privados.');
+    message.author.send('Esto es una prueba de Mensaje Privado.');
+   }
+
+   if (message.content.startsWith(prefix + 'habilidades')) {
+     message.author.send(`**Habilidades** - \nHay tres habilidades que puedes mejorar: Fuerza, Inteligencia y Resistencia, estas habilidades se mejoran gastando dinero ($) u oro, esta última opción es mucho más rápida que la primera, también existe la opción de comprar un boost temporal de 100 puntos de una habilidad, es muy caro pero a al vez muy útil sobre todo en tiempos de guerra. \n \n**Fuerza** - Esta habilidad aumenta tu daño en guerras, tu efectividad trabajando en fábricas y reduce el coste de producir objetos y armamento. \n \n**Inteligencia** - Aumenta el máximo de experiencia laboral que puedes acumular, aumenta un poco tu daño en guerra y reduce los costes de producción. El nivel 100 de INteligencia te permite trabajar en los *Departamentos*.\n \n**Resistencia** - Reduce la energía necesaria para trabajar y participar en guerras, aumenta levemente tu daño en guerra y amplía la capacidad del almacén un 1% por cada punto de habilidad.`);
+
+     message.channel.send('Mira tus mensajes privados.');
+   }
+
+   if (message.content.startsWith(prefix + 'setgame')) {
+    client.user.setGame(argresult);
+   }
+
 });
 
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'bienvenida');
+  if (!channel) return;
+  channel.send(`Bienvenido ${member}, di tu nombre de RR y el partido al que perteneces (PCO, PCA o PCE)`);
+});
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
