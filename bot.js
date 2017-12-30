bf78b2a15738e5a6fb7497a56dd485a6f3e00c04
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2017-12-30 19:01:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,8 +14,37 @@ client.on('message', message => {
    }
 
    if (message.content.startsWith(prefix + 'help')) {
-     message.channel.send(`help - Muestra una lista de todos los códigos. \nhabilidades - Muestra un resumen de la habilidades.\nprueba - Envía un mensaje privado de prueba. `);
-   }
+     message.channel.send({embed: {
+      color: 3447003,
+      author: {
+        name: client.user.username,
+        icon_url: client.user.avatarURL
+      },
+      title: "Lista de Comandos",
+      url: "http://google.com",
+      description: "Esta es una lista con los comandos del servidor y lo que hacen.",
+      fields: [{
+          name: "`help`",
+          value: "Muestra una lista con los comandos y sus funciones."
+        },
+        {
+          name: "`habilidades`",
+          value: "Muestra una breve explicación de las habilidades (o stats) en RivalRegions"
+        },
+        {
+          name: "`prueba`",
+          value: "Envía un mensaje privado de prueba para comprobar el estado del bot."
+        }
+      ],
+      timestamp: new Date(),
+      footer: {
+        icon_url: client.user.avatarURL,
+        text: "© Example"
+      }
+    }
+
+  });
+ }
 
    if (message.content.startsWith(prefix + 'prueba')) {
     message.channel.send('Mira tus mensajes privados.');
@@ -23,9 +52,37 @@ client.on('message', message => {
    }
 
    if (message.content.startsWith(prefix + 'habilidades')) {
-     message.author.send(`**Habilidades** - \nHay tres habilidades que puedes mejorar: Fuerza, Inteligencia y Resistencia, estas habilidades se mejoran gastando dinero ($) u oro, esta última opción es mucho más rápida que la primera, también existe la opción de comprar un boost temporal de 100 puntos de una habilidad, es muy caro pero a al vez muy útil sobre todo en tiempos de guerra. \n \n**Fuerza** - Esta habilidad aumenta tu daño en guerras, tu efectividad trabajando en fábricas y reduce el coste de producir objetos y armamento. \n \n**Inteligencia** - Aumenta el máximo de experiencia laboral que puedes acumular, aumenta un poco tu daño en guerra y reduce los costes de producción. El nivel 100 de INteligencia te permite trabajar en los *Departamentos*.\n \n**Resistencia** - Reduce la energía necesaria para trabajar y participar en guerras, aumenta levemente tu daño en guerra y amplía la capacidad del almacén un 1% por cada punto de habilidad.`);
 
-     message.channel.send('Mira tus mensajes privados.');
+     message.channel.send({embed: {
+      color: 3447003,
+      author: {
+        name: client.user.username,
+        icon_url: client.user.avatarURL
+      },
+      title: "Habilidades",
+      url: "http://google.com",
+      description: "Existen 3 habilidades en el juego: Fuerza, Inteligencia y Resistencia, estas habilidades se mejoran gastando dinero ($) u oro, esta última opción es mucho más rápida que la primera, también existe la opción de comprar un boost temporal de 100 puntos de una habilidad, es muy caro pero a al vez muy útil sobre todo en tiempos de guerra.",
+      fields: [{
+          name: "`Fuerza`",
+          value: "Esta habilidad aumenta tu daño en guerras, tu efectividad trabajando en fábricas y reduce el coste de producir objetos y armamento."
+        },
+        {
+          name: "`Inteligencia`",
+          value: "Aumenta el máximo de experiencia laboral que puedes acumular, aumenta un poco tu daño en guerra y reduce los costes de producción. El nivel 100 de Inteligencia te permite trabajar en los *Departamentos*."
+        },
+        {
+          name: "`Aguante`",
+          value: "Reduce la energía necesaria para trabajar y participar en guerras, aumenta levemente tu daño en guerra y amplía la capacidad del almacén un 1% por cada punto de habilidad."
+        }
+      ],
+      timestamp: new Date(),
+      footer: {
+        icon_url: client.user.avatarURL,
+        text: "© Example"
+      }
+    }
+
+  });
    }
 
    if (message.content.startsWith(prefix + 'setgame')) {
